ccafd849f1902e54823c8e059374ab5f1754a3fd
mogu <443115355@qq.com>
2014-07-29 12:32:08 +0800

    Convert \t to space

--- ThreadSafeQueue.hpp
+++ ThreadSafeQueue.hpp
@@ -7,65 +7,65 @@
 template<class T, class Container = std::deque<T>>
 class ThreadSafeQueue
 {
-	using value_type      = typename Container::value_type;
-	using reference       = typename Container::reference;
-	using const_reference = typename Container::const_reference;
+    using value_type      = typename Container::value_type;
+    using reference       = typename Container::reference;
+    using const_reference = typename Container::const_reference;
     using size_type       = typename Container::size_type;
 
 public:
-	bool Empty() const
+    bool Empty() const
     {
-		std::lock_guard<std::mutex> lock(_mutex);
-		return _queue.empty();
-	}
+        std::lock_guard<std::mutex> lock(_mutex);
+        return _queue.empty();
+    }
 
     size_type Size() const
     {
-		std::lock_guard<std::mutex> lock(_mutex);
+        std::lock_guard<std::mutex> lock(_mutex);
         return _queue.size();
     }
 
-	void Push(value_type&& val)
+    void Push(value_type&& val)
     {
-		std::lock_guard<std::mutex> lock(_mutex);
-		_queue.push(val);
-	}
+        std::lock_guard<std::mutex> lock(_mutex);
+        _queue.push(val);
+    }
 
-	void Push(const value_type& val)
+    void Push(const value_type& val)
     {
-		std::lock_guard<std::mutex> lock(_mutex);
-		_queue.push(val);
-	}
+        std::lock_guard<std::mutex> lock(_mutex);
+        _queue.push(val);
+    }
 
-	void Pop()
+    void Pop()
     {
-		std::lock_guard<std::mutex> lock(_mutex);
-		_queue.pop();
-	}
+        std::lock_guard<std::mutex> lock(_mutex);
+        _queue.pop();
+    }
 
     T GetTop()
     {
-		std::lock_guard<std::mutex> lock(_mutex);
+        std::lock_guard<std::mutex> lock(_mutex);
         T item{std::move(_queue.front())};
         _queue.pop();
         return item;
     }
 
-	const_reference Front() const
+    const_reference Front() const
     {
-		std::lock_guard<std::mutex> lock(_mutex);
-		return _queue.front();
-	}
+        std::lock_guard<std::mutex> lock(_mutex);
+        return _queue.front();
+    }
 
-	const_reference Back() const
+    const_reference Back() const
     {
-		std::lock_guard<std::mutex> lock(_mutex);
-		return _queue.back();
-	}
+        std::lock_guard<std::mutex> lock(_mutex);
+        return _queue.back();
+    }
 
 private:
-	std::queue<T, Container> _queue;
-	mutable std::mutex       _mutex;
+    std::queue<T, Container> _queue;
+    mutable std::mutex       _mutex;
 };
 
 #endif // !THREAD_SAFE_QUEUE_HPP
